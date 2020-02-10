6591035cffad7b6325c5053600b18a020f8e7e49
dz210 <59654639+dz210@users.noreply.github.com>
2020-02-09 22:18:35 -0500

    Shopping Cart Project Submit

---
+++ shoppingcart.py
@@ -0,0 +1,82 @@
+#shopping_cart.py
+
+#from pprint import pprint
+
+products = [
+    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
+    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
+    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
+    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
+    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
+    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
+    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
+    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
+    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
+    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
+    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
+    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
+    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
+    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
+    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
+    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
+    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
+    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
+    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
+    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
+] 
+# based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+    Source: https://github.com/prof-rossetti/intro-to-python/blob/master/notes/python/datatypes/numbers.md#formatting-as-currency
+    Param: my_price (int or float) like 4000.444444
+    Example: to_usd(4000.444444)
+    Returns: $4,000.44
+    """
+    return f"${my_price:,.2f}"  # > $12,000.71
+
+total_price = 0
+selected_ids = []
+
+while True:
+    selected_id = input("Please input a product identifier: ")
+
+    if selected_id == "DONE":
+        break
+
+    elif ((selected_id is not "1") and (selected_id is not "2") and (selected_id is not "3") and (selected_id is not "4") and (selected_id is not "5") and (selected_id is not "6") and (selected_id is not "7") and (selected_id is not "8") and (selected_id is not "9") and (selected_id != "10") and (selected_id != "11") and (selected_id != "12") and (selected_id != "13") and (selected_id != "14") and (selected_id != "15") and (selected_id != "16") and (selected_id != "17") and (selected_id != "18") and (selected_id != "19") and (selected_id != "20")):
+        print("Hey, are you sure that product identifier is correct? Please try again!")
+
+    else: 
+        selected_ids.append(selected_id)
+
+for selected_id in selected_ids:
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
+
+        tax = 0.0875*total_price
+        total_payment = tax + total_price
+
+import datetime
+now = datetime.datetime.now()
+
+print("---------")
+print("Dongxu's Grocery Store")
+print("PHONE NUMBER: 203-291-9993")
+print("WEBSITE: www.mikezhanggrocerystore.com")
+print("---------")
+print("CHECKOUT AT: " + str(now.strftime("%Y-%m-%d %H:%M:%S")))
+print("---------")
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    print("SELECTED PRODUCT: " + matching_product["name"] + "   " + '${:.2f}'.format(matching_product["price"]))
+print("---------")
+print("SUBTOTAL: " + '${:.2f}'.format(round(total_price, 2)))
+print("TAX: " + '${:.2f}'.format(round(tax, 2)))
+print("TOTAL: " + '${:.2f}'.format(round(total_payment,2)))
+print("---------")
+print("THANK YOU! SEE YOU AGAIN SOON!")
+print("---------")
